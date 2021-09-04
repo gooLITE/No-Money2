76cb912bb424d7123f851111819e9e63bd452ba2
gooLITE <goolitesum@gmail.com>
2021-09-04 17:44:20 +0800

    Initial Commit

---
+++ No-Money2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  No-Money2
+//
+//  Created by Yu Sum Yung on 4/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
